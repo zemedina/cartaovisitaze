3a6613b2838a1d4211f9894bc1677749ddefb631
zemedina <zemedinaoficial@gmail.com>
2023-03-22 19:29:04 -0300

    adicaso
    adicao

---
+++ script.js
@@ -0,0 +1,6 @@
+//$('document').ready(function(){});
+ // var video = document.getElementById("video"); 
+ // video.play();
+var vid = document.getElementById("video");
+vid.autoplay = true ;
+vid.load()
